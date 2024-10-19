5891e37fb1c7cb016a63ac650a518255991f3be2
Karlatemp <i@karla.moe>
2024-10-19 16:18:49 +0800

    gradle plugin publishing setup

--- modules/gradle-plugin/build.gradle.kts
+++ modules/gradle-plugin/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     java
     `groovy-gradle-plugin`
+
+    id("com.gradle.plugin-publish") version "1.3.0"
 }
 
 repositories {
@@ -23,16 +25,27 @@ dependencies {
 }
 
 gradlePlugin {
+    website.set("https://github.com/Karlatemp/maven-central-publish")
+    vcsUrl.set("https://github.com/Karlatemp/maven-central-publish")
+
     testSourceSets(sourceSets.test.get())
 
     plugins {
         register("maven-publishing") {
             id = "moe.karla.maven-publishing"
             implementationClass = "moe.karla.maven.publishing.MavenPublishingPlugin"
+
+            displayName = "Maven Central Publishing"
+            description = "Publishing your software to Maven Central"
+            tags.set(listOf("signing", "publishing"))
         }
         register("publishing-signing") {
             id = "moe.karla.signing-setup"
             implementationClass = "moe.karla.maven.publishing.SigningSetupPlugin"
+
+            displayName = "Gradle Signing Setup"
+            description = "Fast setup your publications signing"
+            tags.set(listOf("signing", "publishing"))
         }
     }
 }
